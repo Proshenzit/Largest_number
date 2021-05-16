838d9082606c8d2db7c067b2757f65306ddadeb5
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-15 20:20:36 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,39 @@
+// var Marks = prompt("Enter  your Marks");
+
+
+var num1=prompt("Enter num1: ");
+
+var num2=prompt("Enter num2: ");
+
+var num3=prompt("Enter num3: ");
+
+
+if(num1 > num2 && num1 > num3)
+
+console.log("Large Number = "+num1);
+
+  else if(num2 > num1 && num2 > num3)
+
+console.log("Large Number = "+num2);
+
+
+else 
+
+console.log("Large Number = "+num3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
